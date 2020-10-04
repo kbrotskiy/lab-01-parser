961327951e96503178317638157f20b6c19b11fe
Kirill Brotskiy <56027225+kbrotskiy@users.noreply.github.com>
2020-10-05 00:44:12 +0300

    Add files via upload

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/kirillbrockij/АЯ-3/111/lab-01-parser
+# Build directory: /Users/kirillbrockij/АЯ-3/111/lab-01-parser/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "/Users/kirillbrockij/АЯ-3/111/lab-01-parser/cmake-build-debug/tests")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/kirillbrockij/АЯ-3/111/lab-01-parser/CMakeLists.txt;65;add_test;/Users/kirillbrockij/АЯ-3/111/lab-01-parser/CMakeLists.txt;0;")
